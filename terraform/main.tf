8c5bcc35a9b3d26ee9a03a6c13afa340d49fe98b
Stephen Pride <spride@gmail.com>
2023-07-08 17:47:50 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_ssm_parameter" "secret" {
+  name        = "/temp/data"
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = "supersecret"
+}
