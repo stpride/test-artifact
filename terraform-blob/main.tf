be1348ac32483c3fc949c9ba1c476b39d83730f1
Stephen Pride <spride@gmail.com>
2023-07-26 16:03:09 -0500

    Update main.tf

--- terraform-blob/main.tf
+++ terraform-blob/main.tf
@@ -4,7 +4,6 @@ variable "invoke_functions" {
 }
 
 data "aws_iam_policy_document" "invoke_functions" {
-  count = length(var.invoke_functions) > 0 ? 1 : 0
   statement {
     sid = "InvokeFunctions"
     actions = [
@@ -19,5 +18,5 @@ resource "aws_iam_policy" "invoke_functions" {
   count = length(var.invoke_functions) > 0 ? 1 : 0
   name   = "invoke-functions"
   path   = "/"
-  policy = data.aws_iam_policy_document.invoke_functions[0].json
+  policy = data.aws_iam_policy_document.invoke_functions.json
 }
