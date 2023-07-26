9abe79e4bf1493d53800c8af928db7657308c452
Stephen Pride <spride@gmail.com>
2023-07-26 15:54:24 -0500

    Update main.tf

--- terraform-blob/main.tf
+++ terraform-blob/main.tf
@@ -3,31 +3,6 @@ variable "invoke_functions" {
   default = []
 }
 
-locals {
-  lambdas = flatten([for v in var.invoke_functions : "arn:aws:lambda:us-west-2:123456::function:${v}"])
-}
-
-#resource aws_iam_policy lambda {
-#  count       = length(var.invoke_functions) > 0 ? 1 : 0
-#  name        = "blob-invoke-functions-policy"
-#  description = "blob-invoke-functions-policy"
-
-#  policy = <<-EOF
-#  {
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Effect": "Allow",
-#        "Action": [
-#          "lambda:InvokeFunctions"
-#        ],
-#        "Resource": [${local.lambdas}]
-#      }
-#    ]
-#  }
-#  EOF
-#}
-
 data "aws_iam_policy_document" "invoke_functions" {
   count = length(var.invoke_functions) > 0 ? 1 : 0
   statement {
@@ -38,4 +13,10 @@ data "aws_iam_policy_document" "invoke_functions" {
     effect = "Allow"
     resources = flatten([for v in var.invoke_functions : "arn:aws:lambda:us-west-2:123456::function:${v}"])
   }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example_policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.invoke_functions.json
 }
