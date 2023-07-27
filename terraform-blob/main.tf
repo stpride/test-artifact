732347f8c6d716665e5af118fa4deae509454673
Stephen Pride <spride@gmail.com>
2023-07-27 09:55:58 -0500

    Update main.tf

--- terraform-blob/main.tf
+++ terraform-blob/main.tf
@@ -1,22 +1,24 @@
-variable "invoke_functions" {
-  type    = list(string)
-  default = []
-}
+#variable "invoke_functions" {
+#  type    = list(string)
+#  default = []
+#}
 
-data "aws_iam_policy_document" "invoke_functions" {
-  statement {
-    sid = "InvokeFunctions"
-    actions = [
-      "lambda:InvokeFunctions"
-    ]
-    effect = "Allow"
-    resources = flatten([for v in var.invoke_functions : "arn:aws:lambda:us-west-2:123456::function:${v}"])
-  }
-}
+#data "aws_iam_policy_document" "invoke_functions" {
+#  statement {
+#    sid = "InvokeFunctions"
+#    actions = ["lambda:InvokeFunction"]
+#    effect = "Allow"
+#    resources = flatten([for v in var.invoke_functions : "arn:aws:lambda:us-west-2:123456:function:${v}"])
+#  }
+#}
 
-resource "aws_iam_policy" "invoke_functions" {
-  count = length(var.invoke_functions) > 0 ? 1 : 0
-  name   = "invoke-functions"
-  path   = "/"
-  policy = data.aws_iam_policy_document.invoke_functions.json
+#resource "aws_iam_policy" "invoke_functions" {
+#  count = length(var.invoke_functions) > 0 ? 1 : 0
+#  name   = "invoke-functions"
+#  policy = data.aws_iam_policy_document.invoke_functions.json
+#}
+
+variable "s3_buckets" {
+  type = map
+  default = {}
 }
