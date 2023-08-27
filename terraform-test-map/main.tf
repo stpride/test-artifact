31218aec72b205da0d409e3567c1260d642b99d8
Stephen Pride <spride@gmail.com>
2023-08-27 15:58:01 -0500

    Update main.tf

--- terraform-test-map/main.tf
+++ terraform-test-map/main.tf
@@ -10,18 +10,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-variable subs {
-  type = list(object({
-    p = string
-    v = string
-  }))
-  default = []
-}
-
-resource "aws_ssm_parameter" "secret" {
-  for_each    = { for s in var.subs: s.p => s }
-  name        = each.value.p
-  description = "The parameter description"
-  type        = "SecureString"
-  value       = each.value.v
+module test {
+  source = "git@github.com:stpride/module-aws-test1@main"
+  subs = [
+    {
+      p = "/alt/test"
+      v = "mystring"
+    }
+  ]
 }
